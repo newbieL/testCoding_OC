9beccea4074dc759a8b28336697ec9e1f3d31ce4
Ace <Ace_Z@lixianshengdemacbook-pro.local>
2016-07-23 13:49:39 +0800

    Initial Commit

---
+++ test_one/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_one
+//
+//  Created by Ace_Z on 16/7/23.
+//  Copyright © 2016年 newbieTask. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
